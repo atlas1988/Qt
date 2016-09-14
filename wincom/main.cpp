0223067953a61a324fa55ff16c46d4b789c7ee14
atlas <atlas_li@126.com>
2016-09-14 18:35:51 +0800

    wincom resolve chinese compatibility error

--- wincom/main.cpp
+++ wincom/main.cpp
@@ -1,9 +1,11 @@
 #include "wincommainwindow.h"
-#include <QApplication>
+#include <QApplication> // 加入头文件 支持中文
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    // 使程序可以处理中文
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName(QTextCodec::codecForLocale()->name()));
     WincomMainWindow w;
     w.show();
 
